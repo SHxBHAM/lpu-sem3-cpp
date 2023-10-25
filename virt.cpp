79de6cbd136e69bd1a9ae2052d873283066720da
SHxBHAM <shubhammishra41880@gmail.com>
2023-10-25 15:38:18 +0530

    class notes

---
+++ virt.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class base{
+    public:
+    base(){
+        cout<<"Constructing base"<<endl;
+    }
+    ~base(){
+        cout<<"destructing base"<<endl;
+    }
+};
+
+class derived1:public base{
+    public:
+    derived1(){
+        cout<<"constructing derived"<<endl;
+    }
+    ~derived1(){
+        cout<<"destructing derived"<<endl;
+    }
+     
+};
+int main(){
+    base *b = new derived1;
+    delete b;
+    return 0;
+}
